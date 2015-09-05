373d8d6093b633f70c1d7748bcbe7b5a8288beba
Josh Goldberg <josh@fullscreenmario.com>
2015-09-05 15:12:49 -0700

    Fixed parts of GLSC for meta usage
    '{' kills the lines...

--- Source/GLSC.ts
+++ Source/GLSC.ts
@@ -1,4 +1,4 @@
-/// <reference path="Language.ts" />
+/// <reference path='Language.ts' />
 
 module GLS {
     export class GLSC {
@@ -13,51 +13,56 @@ module GLS {
                 "(": ")"
             };
         }
+        
+        
+        /*
+        Core parsing
+        */
 
         public parseCommands(language: Language, commandsRaw: string[]): string {
-            if (commandsRaw.length === 0 || (commandsRaw.length === 1 && commandsRaw[0].length === 0)) {
+            if (commandsRaw.length == 0 || (commandsRaw.length == 1 && commandsRaw[0].length == 0)) {
                 return "";
             }
 
-            var output: string = "",
-                command: any[],
-                numTabs: number = 0,
-                lastTabRequest: number = 0,
-                lastLineSkipped: boolean = false,
-                i: number,
-                j: number;
+            var output: string = "";
+            var numTabs: number = 0;
+            var lastTabRequest: number = 0;
+            var lastLineSkipped: boolean = false;
+            var command: any[];
+            var i: number;
+            var j: number;
 
             for (i = 0; i < commandsRaw.length; i += 1) {
                 command = this.parseCommand(language, commandsRaw[i], false);
-
+                
                 // Each command is an even-length [string, int, ...]
                 for (j = 0; j < command.length; j += 2) {
                     // Special case: a blank line after an inline command is ignored
                     // This is useful for things like lambda types that aren't in every language
-                    if (lastTabRequest === Language.INT_MIN && command.length === 2 && command[0] === "") {
+                    if (lastTabRequest == Language.INT_MIN && command.length == 2 && command[0] == "") {
                         lastLineSkipped = true;
                         continue;
                     }
 
-                    if (command[1] === Language.INT_MIN) {
-                        if (command[0] !== "") {
+                    if (command[1] == Language.INT_MIN) {
+                        if (command[0] != "") {
                             output += " " + command[0];
                         }
                     } else {
                         // Just "\0" changes numTabs without adding a line
-                        if (command[j] !== "\0" && !lastLineSkipped) {
+                        if (command[j] != "\0" && !lastLineSkipped) {
                             output += "\n";
                         }
 
                         if (command[j + 1] < 0) {
                             numTabs += command[j + 1];
                             output += this.generateTabs(numTabs);
-                            if (command[j] !== "\0") {
+                            if (command[j] != "\0") {
                                 output += command[j];
                             }
                         } else {
                             output += this.generateTabs(numTabs);
-                            if (command[j] !== "\0") {
+                            if (command[j] != "\0") {
                                 output += command[j];
                             }
                             numTabs += command[j + 1];
@@ -72,40 +77,38 @@ module GLS {
             return output;
         }
 
-        public parseCommand(language: Language, commandRaw: string, isInline?: boolean): any[] {
+        public parseCommand(language: Language, commandRaw: string, isInline: boolean): any[] {
             if (this.isStringSpace(commandRaw)) {
                 return ["", 0];
             }
 
-            var colonIndex = commandRaw.indexOf(":"),
-                result: any[],
-                functionArgs: string[],
-                functionName: string,
-                argumentsRaw: string,
-                colonIndex: number;
+            var colonIndex: number = commandRaw.indexOf(":");
+            var result: any[];
+            var functionArgs: string[];
+            var argumentsRaw: string;
             
             // Arguments only exist if there is a colon separating them from the command
-            if (colonIndex === -1) {
+            if (colonIndex == -1) {
                 functionName = this.trimString(commandRaw);
                 functionArgs = [];
             } else {
-                functionName = this.trimString(commandRaw.substring(0, colonIndex));
-                argumentsRaw = this.trimString(commandRaw.substring(colonIndex + 1));
+                functionName = this.trimString(commandRaw.substring(substring, 0, colonIndex));
+                argumentsRaw = this.trimString(commandRaw.substring(substring, colonIndex + 1));
                 functionArgs = this.parseArguments(language, argumentsRaw, isInline);
             }
 
             return language.print(functionName, functionArgs, isInline);
         }
 
-        public parseArguments(language: Language, argumentsRaw: string, isInline?: boolean): string[] {
-            var numArgs: number = 0,
-                argumentsConverted: string[],
-                argument: string,
-                starter: string,
-                end: number,
-                i: number;
-
-            // Until native array pushing is suported, this is required...
+        public parseArguments(language: Language, argumentsRaw: string, isInline: boolean): string[] {
+            var numArgs: number = 0;
+            var argumentsConverted: string[];
+            var argument: string;
+            var starter: string;
+            var end: number;
+            var i: number;
+            
+            // Until native array pushing is supported, this is required...
             for (i = 0; i < argumentsRaw.length; i += 1) {
                 starter = argumentsRaw[i];
 
@@ -113,14 +116,14 @@ module GLS {
                     continue;
                 }
 
-                if (starter == '{' || starter == '(') {
+                if (this.SearchEnds.hasOwnProperty(starter)) {
                     end = this.findSearchEnd(argumentsRaw, starter, i);
                     i += 1;
                 } else {
                     end = this.findNextSpace(argumentsRaw, i);
                 }
 
-                if (end === -1) {
+                if (end == -1) {
                     end = argumentsRaw.length;
                 }
 
@@ -138,116 +141,121 @@ module GLS {
                     continue;
                 }
 
-                if (starter == '{' || starter == '(') {
+                if (this.SearchEnds.hasOwnProperty(starter)) {
                     end = this.findSearchEnd(argumentsRaw, starter, i);
                     i += 1;
                 } else {
                     end = this.findNextSpace(argumentsRaw, i);
                 }
 
-                if (end === -1) {
+                if (end == -1) {
                     end = argumentsRaw.length;
                 }
 
-                argument = argumentsRaw.substr(i, end - i);
+                argument = argumentsRaw.substring(i, end - i);
                 i = end;
 
-                if (starter === '{') {
-                    argument = this.parseCommand(language, argument, true)[0];
-                }
-
-                argumentsConverted[numArgs] = argument;
-                numArgs += 1;
+                if (starter == '{' } undefined) {
+                argument = this.parseCommand(language, argument, true)[0];
             }
 
+            argumentsConverted[numArgs] = argument;
+            numArgs += 1;
+        }
+            
             return argumentsConverted;
-        }
-
-
-        /* Private utilities
+}
+        
+        
+        /*
+        Private utilities
         */
-
+        
         private generateTabs(numTabs: number): string {
-            var numTabsActual: number = numTabs * 4,
-                output: string = "",
-                i: number;
+    var numTabsActual: number = numTabs * 4;
+    var output: string = "";
+    var i: number;
 
-            for (i = 0; i < numTabsActual; i += 1) {
-                output += " ";
-            }
+    for (i = 0; i < numTabsActual; i += 1) {
+        output += " ";
+    }
 
-            return output;
-        }
-
+    return output;
+}
+        
         private isStringSpace(text: string): boolean {
-            for (var i: number = 0; i < text.length; i += 1) {
-                if (!this.isCharacterSpace(text[i])) {
-                    return false;
-                }
-            }
-
-            return true;
+    var i: number;
+    for (i = 0; i < text.length; i += 1) {
+        if (!this.isCharacterSpace(text[i])) {
+            return false;
         }
+    }
 
+    return true;
+}
+        
         private isCharacterSpace(character: string): boolean {
-            return character === ' ' || character === '\r' || character === '\n';
-        }
-
+    return character == ' ' || character == '\r' || character == '\n';
+}
+        
         private findNextSpace(haystack: string, start: number): number {
-            for (var i: number = start + 1; i < haystack.length; i += 1) {
-                if (this.isCharacterSpace(haystack[i])) {
-                    return i;
-                }
-            }
+    var i: number;
 
-            return haystack.length;
+    for (i = start + 1; i < haystack.length; i += 1) {
+        if (this.isCharacterSpace(haystack[i])) {
+            return i;
         }
+    }
 
+    return haystack.length;
+}
+        
         private findSearchEnd(haystack: string, starter: string, start: number): number {
-            var numStarts: number = 1,
-                ender: string = this.SearchEnds[starter],
-                current: string,
-                i: number;
+    var ender: string = this.SearchEnds[starter];
+    var numStarts: number = 1;
+    var current: string;
+    var i: number;
 
-            for (i = start + 1; i < haystack.length; i += 1) {
-                current = haystack[i];
+    for (i = start + 1; i < haystack.length; i += 1) {
+        current = haystack[i];
 
-                if (current === starter) {
-                    numStarts += 1;
-                }
-                else if (current === ender) {
-                    numStarts -= 1;
-                    if (numStarts < 1) {
-                        return i;
-                    }
-                }
+        if (current == starter) {
+            numStarts += 1;
+        } else if (current == ender) {
+            numStarts -= 1;
+            if (numStarts < 1) {
+                return i;
             }
-
-            return -1;
         }
+    }
 
+    return -1;
+}
+        
         private trimString(text: string): string {
-            return this.trimStringLeft(this.trimStringRight(text));
-        }
-
+    return this.trimStringLeft(this.trimStringRight(text));
+}
+        
         private trimStringLeft(text: string): string {
-            for (var i: number = 0; i < text.length; i += 1) {
-                if (!this.isCharacterSpace(text[i])) {
-                    return text.substr(i);
-                }
-            }
-
-            return "";
+    var i: number;
+    for (i = 0; i < text.length; i += 1) {
+        if (!this.isCharacterSpace(text[i])) {
+            return text.substring(i);
         }
+    }
 
+    return "";
+}
+        
         private trimStringRight(text: string): string {
-            for (var i: number = text.length - 1; i >= 0; i -= 1) {
-                if (!this.isCharacterSpace(text[i])) {
-                    return text.substr(0, i + 1);
-                }
-            }
-
-            return "";
+    var i: number;
+    for (i = text.length - 1; i > 0; i += -1) {
+        if (!this.isCharacterSpace(text[i])) {
+            return text.substring(0, i + 1);
         }
+    }
+
+    return "";
+}
     }
 }
